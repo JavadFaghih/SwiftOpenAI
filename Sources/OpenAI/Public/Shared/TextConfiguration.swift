47fdbff10581f7f39f6e912ab0e97a6b65ef7d75
Javad Faghih <Javad.4145@gmail.com>
2025-09-24 15:13:05 +0330

    verbosity added

--- Sources/OpenAI/Public/Shared/TextConfiguration.swift
+++ Sources/OpenAI/Public/Shared/TextConfiguration.swift
@@ -57,6 +57,7 @@ public enum FormatType: Codable {
     switch self {
     case .text:
       try container.encode("text", forKey: .type)
+      try container.encode("verbosity", forKey: .verbosity)
 
     case .jsonSchema(let schema, let name):
       try container.encode("json_schema", forKey: .type)
